2fe8af8d85c3bd7839598d31c123501dcfa48a4d
Tarunj05 <jaiswalt54@gmail.com>
2026-06-19 01:44:35 +0530

    added logout feature with blacklisting and a getMe feature

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const authController = require("../controllers/auth.controller")
+const authMiddleware = require("../middlewares/auth.middleware")
 //creating a router object
 const authRouter = express.Router()
 // creating api route here
@@ -22,6 +23,23 @@ authRouter.post("/register",authController.registerUserController)
 
 authRouter.post("/login" , authController.loginUserController)
 
+/**
+ * @route GET /api/auth/logout
+ * @description clear token from user cookie and add the token in blacklist
+ * @access Public
+ */
 
+authRouter.get("/logout",authController.logoutUserController)
+
+// now we are gonna create a getMe api, to fetch user details who request it
+// for that we need a middleware that tells us which user is logged in
+
+/**
+ * @route : GET /api/auth/get-me
+ * @description : get the current logged in user details
+ * @access Private
+ */
+
+authRouter.get("/get-me",authMiddleware.authUser,authController.getMecontroller)
 
 module.exports = authRouter
